f37046e113fcdf01271417c247c402d761341b38
Koji Miwa <koji.miwa.c4@tohoku.ac.jp>
2025-04-28 15:04:15 +0900

    Modify UserCFT for PHC and PiID

--- usr/UserCFT.cc
+++ usr/UserCFT.cc
@@ -33,7 +33,7 @@
 // #define TimeCut    1 // in cluster analysis
 #define FHitBranch 0 // make FiberHit branches (becomes heavy)
 #define HodoHitPos 0
-#define ForPHC     1
+#define ForPHC     0
 
 namespace
 {
